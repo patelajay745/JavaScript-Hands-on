866f15407f7f98db1c52fa42f35f983e145a4a5a
ajaypatel <patel.ajay745@gmail.com>
2024-05-02 21:46:07 -0400

    added datatype and conversion operation

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,42 @@
+// Number data type
+let num = 42;
+
+// String data type
+let str = "Hello, world!";
+
+// Boolean data type
+let bool = true;
+
+// Undefined data type
+let undef;
+
+// Null data type
+let nul = null;
+
+// Object data type
+let obj = {
+  name: "John",
+  age: 30,
+  isStudent: false
+};
+
+// Array data type
+let arr = [1, 2, 3, "apple", true];
+
+// Function data type
+function greet(name) {
+  return "Hello, " + name + "!";
+}
+
+// Symbol data type
+let sym = Symbol("foo");
+
+console.log("Number:", num);
+console.log("String:", str);
+console.log("Boolean:", bool);
+console.log("Undefined:", undef);
+console.log("Null:", nul);
+console.log("Object:", obj);
+console.log("Array:", arr);
+console.log("Function:", greet("Alice"));
+console.log("Symbol:", sym);
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+// Conversion from string to number
+let strNum = "42";
+let num = Number(strNum);
+
+// Conversion from number to string
+let numStr = 42;
+let str = String(numStr);
+
+// Conversion from string to boolean
+let strBool = "true";
+let bool = Boolean(strBool);
+
+// Conversion from boolean to string
+let boolStr = true;
+let str2 = String(boolStr);
+
+// Conversion from number to boolean
+let numBool = 0;
+let bool2 = Boolean(numBool);
+
+// Conversion from boolean to number
+let boolNum = true;
+let num2 = Number(boolNum);
+
+console.log("String to Number:", num);
+console.log("Number to String:", str);
+console.log("String to Boolean:", bool);
+console.log("Boolean to String:", str2);
+console.log("Number to Boolean:", bool2);
+console.log("Boolean to Number:", num2);
+
+console.log(typeof num);
